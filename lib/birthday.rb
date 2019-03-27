0eebf7d4d6fef63329098d38bb560a4b57501f04
Nosa Okundaye <okundaye.preciousnosa@gmail.com>
2019-03-27 02:33:04 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -14,7 +14,7 @@ end
 def age_appropriate_birthday(kids_birthday)
   kids_birthday.each do |name, age|
     if age <= 12
-      puts "Happy Birthday #{name}! You are now #{age} years old!"
+      puts "Happy Birthday #{name}! You are now #{age} years old!\n"
     else
       puts "You are too old for this."
     end
